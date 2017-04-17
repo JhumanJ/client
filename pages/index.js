d69508733d35776d7ac5708ffd00c8d67b54976b
JhumanJ <julien@nahum.net>
2017-04-17 16:53:31 +0100

    Recreating all pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Button from '../components/Button'
 import Layout from '../components/Layout'
+import Link from 'next/link'
 
 export default () => (
     <Layout>
         <p>PEACH Cancer!</p>
         <Button>Press Me :)</Button>
+
+        <Link href="/referrals"><a>here</a></Link>
     </Layout>
 )
---
+++ pages/live-mdt.js
@@ -0,0 +1,3 @@
+export default () => (
+  <p>Live MDT</p>
+)
