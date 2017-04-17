96f1c9cbb4cec647a1af1e7b1856a5a676a1ef35
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-04-17 20:55:26 +0100

    setup redux

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
+import React from 'react'
+import withRedux from 'next-redux-wrapper'
+import {initStore, startClock} from '../store'
 import Layout from '../components/Layout'
 import {P, Heading} from '../components/Text'
 
-export default () => (
-    <Layout>
-        <Heading>Welcome to PEACH Cancer!</Heading>
-        <P>Some body text goes here</P>
-    </Layout>
-)
+class Index extends React.Component {
+    static getInitialProps({store, isServer}) {
+        store.dispatch({type: 'TICK', light: !isServer, ts: Date.now()})
+        return {isServer}
+    }
+
+    componentDidMount() {
+        this.timer = this.props.dispatch(startClock())
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer)
+    }
+
+    render() {
+        return(
+            <Layout>
+                <Heading>Welcome to PEACH Cancer!</Heading>
+                <P>Some body text goes here</P>
+            </Layout>
+        )
+    }
+}
+
+export default withRedux(initStore)(Index)
